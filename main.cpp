ecd1d7b867629159c6d5964a47e61ad27f8449b4
Geppy03 <91633933+Geppy03@users.noreply.github.com>
2021-11-11 00:54:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ int main(){
           cin>>n;
  if(n>=2) {
      cout << "1" << endl;
-     for (int i = 0; i <= n; i++){
+     for (int i = 0; i<=n; i++){
          f = a + b;
-     a = b, b = f;
+     a = b;
+     b = f;
      cout << f << endl;
  }
  }else{
