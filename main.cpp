ff50ef5363e7160693898384413e132c873a0b17
Geppy03 <91633933+Geppy03@users.noreply.github.com>
2021-11-11 00:45:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int main() {
- int n=0, i=0, a=i, b=1, f=0;
+int main(){                                                      
+   int n=0, i=0, a=i, b=1, f=0;
           cin>>n;
  if(i<=n) {
      cout << "1" << endl;
@@ -10,9 +9,8 @@ int main() {
          f = a + b;
      a = b, b = f;
      cout << f << endl;
- }
  }else{
      cout<<"errore"<<endl;
  }
  return 0;
-} 
+}
