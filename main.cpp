2b8b93e5368c51a28e3bfa87c53b22d7519cf612
Geppy03 <91633933+Geppy03@users.noreply.github.com>
2021-11-11 00:53:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 int main(){                                                     
    int n=0, i=0, a=i, b=1, f=0;
-   cin>>n;
-   if(n>=2) {
+          cin>>n;
+ if(n>=2) {
      cout << "1" << endl;
      for (int i = 0; i <= n; i++){
          f = a + b;
-         a = b;
-         b = f;
+     a = b, b = f;
      cout << f << endl;
-     }
-   }else{
+ }
+ }else{
      cout<<"errore"<<endl;
-   }
-   return 0;
+ }
+ return 0;
 }
