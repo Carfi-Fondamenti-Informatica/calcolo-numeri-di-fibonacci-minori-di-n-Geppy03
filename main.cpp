d46df507b8eb9b637316f833fdec56f2ee98fa75
Geppy03 <91633933+Geppy03@users.noreply.github.com>
2021-11-13 16:08:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int fibonacci(int n) {
     int i=0, p=i, s=1, f=1;
     for(int i=0; i<n; i++) {
-        f=p+s, p=s, s=f;
+        f=p+s; 
+        p=s;
+        s=f;
     }
 
 return f;
